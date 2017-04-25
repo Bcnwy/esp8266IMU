889e087bcf14d8ec59aa006bcf956f793391c552
B_cnwy <benconway@outlook.com>
2017-04-25 17:20:19 +0100

    update

--- src/WebSocketIMU.cpp
+++ src/WebSocketIMU.cpp
@@ -21,10 +21,10 @@ extern "C" {
 #include <WebSocketsClient.h>
 
 #define LED = 2;
-#define BNO055_SAMPLERATE_DELAY_MS (10)
+#define BNO055_SAMPLERATE_DELAY_MS (10000)
 #define MPU_9150_delay (10) // millis between samples 1/freq * 1000
-#define ABS_IMU_OUT false
-#define IMU_OUT true
+#define ABS_IMU_OUT true
+#define IMU_OUT false
 #define refresh_delay (10)
 
 // Update these with values suitable for your network.
@@ -32,7 +32,7 @@ const char *_ssid = "CPT Sensors";
 const char *_password = "crossword";
 const char *_server = "192.168.0.102";
 uint16_t _port = 81;
-long lastMsg = 0, lastacc = 0L, start_loop = 0, end_loop = 0, lastmsg = 0, now = 0;
+unsigned  long lastMsg = 0, lastacc = 0, start_loop = 0, end_loop = 0, lastmsg = 0, now = 0;
 bool ACC_data = false, Q_data = false;
 int sample = 0;
 String data;
@@ -43,8 +43,9 @@ WebSocketsClient webSocket;
 Adafruit_BNO055 bno = Adafruit_BNO055(); // Init Sensor
 MPU9250 myIMU;
 
-// static os_timer_t myTimer;
+os_timer_t myTimer;
 bool socket_connected = false;
+void ABS_IMU();
 /*
 ██ ███    ██ ████████ ███████ ██████  ██████  ██    ██ ██████  ████████
 ██ ████   ██    ██    ██      ██   ██ ██   ██ ██    ██ ██   ██    ██
@@ -57,9 +58,95 @@ bool socket_connected = false;
  * @param pArg [description]
  */
 void timerCallback(void *pArg) {
-
+  //os_timer_disarm(&myTimer);
+  if (socket_connected){
+    ABS_IMU();
+  }
 }
+/*
+ █████  ██████  ███████     ██ ███    ███ ██    ██
+██   ██ ██   ██ ██          ██ ████  ████ ██    ██
+███████ ██████  ███████     ██ ██ ████ ██ ██    ██
+██   ██ ██   ██      ██     ██ ██  ██  ██ ██    ██
+██   ██ ██████  ███████     ██ ██      ██  ██████
+*/
+/**
+ * [ABS_IMU description]
+ */
+void ABS_IMU(void){
+  Q_data = true;
+  //sensors_event_t event;
+  //bno.getEvent(&event);
+  // imu::Vector<3> lineacc =
+  // bno.getVector(Adafruit_BNO055::VECTOR_LINEARACCEL);
+  /* //get euler data
+     float X = event.orientation.x;
+     float Y = event.orientation.y;
+     float Z = event.orientation.z;
 
+     char xbuf[10];
+     char ybuf[10];
+     char zbuf[10];
+     dtostrf( X,3,4,xbuf);
+     dtostrf( Y,3,4,ybuf);
+     dtostrf( Z,3,4,zbuf);
+     sprintf(data, "{\"orientation\":{\"euler\":[%s,%s,%s]}}", xbuf, ybuf,
+     zbuf );
+   */
+  // get Quaternion
+  imu::Quaternion quat = bno.getQuat();
+  long sTime = millis();
+  float qW = quat.w();
+  float qX = quat.x();
+  float qY = quat.y();
+  float qZ = quat.z();
+  char wbuf[10];
+  char xbuf[10];
+  char ybuf[10];
+  char zbuf[10];
+  dtostrf(qW, 3, 4, wbuf);
+  dtostrf(qX, 3, 4, xbuf);
+  dtostrf(qY, 3, 4, ybuf);
+  dtostrf(qZ, 3, 4, zbuf);
+  sprintf(Quaternion, "{\"Quaternion\":{\"w\":\"%s\",\"x\":\"%s\",\"y\":\"%"
+                      "s\",\"z\":\"%s\"},\"Time\":\"%i\"}",
+                      wbuf, xbuf, ybuf, zbuf, sTime);
+
+  webSocket.sendTXT(Quaternion);
+  Serial.println(Quaternion);
+}
+/*
+██ ███    ███ ██    ██
+██ ████  ████ ██    ██
+██ ██ ████ ██ ██    ██
+██ ██  ██  ██ ██    ██
+██ ██      ██  ██████
+*/
+/**
+ * [IMU description]
+ */
+void IMU(){
+  ACC_data = true;
+  myIMU.readAccelData(myIMU.accelCount); // Read the x/y/z adc values
+  myIMU.getAres();
+  // Now we'll calculate the accleration value into actual g's
+  // This depends on scale being set
+  myIMU.ax = (float)myIMU.accelCount[0] * myIMU.aRes; // - accelBias[0];
+  myIMU.ay = (float)myIMU.accelCount[1] * myIMU.aRes; // - accelBias[1];
+  myIMU.az = (float)myIMU.accelCount[2] * myIMU.aRes; // - accelBias[2];
+  char axbuf[10];
+  char aybuf[10];
+  char azbuf[10];
+  dtostrf(myIMU.ax, 3, 4, axbuf);
+  dtostrf(myIMU.ay, 3, 4, aybuf);
+  dtostrf(myIMU.az, 3, 4, azbuf);
+  sprintf(MPU_ACC, "{\"Accelerometer\":{\"x\":\"%s\",\"y\":\"%s\",\"z\":\"%"
+                   "s\"},\"Time\":\"%i\"}",
+          axbuf, aybuf, azbuf, millis());
+  //data = MPU_ACC;
+  webSocket.sendTXT(MPU_ACC);
+  Serial.println(MPU_ACC);
+}
 /*
 ██     ██ ███████ ██████  ███████  ██████   ██████ ██   ██ ███████ ████████
 ██     ██ ██      ██   ██ ██      ██    ██ ██      ██  ██  ██         ██
@@ -74,7 +161,6 @@ void timerCallback(void *pArg) {
  * @param length  [description]
  */
 void webSocketEvent(WStype_t type, uint8_t *payload, size_t length) {
-
   switch (type) {
   case WStype_DISCONNECTED:
     Serial.printf("[WSc] Disconnected!\n");
@@ -103,7 +189,7 @@ void webSocketEvent(WStype_t type, uint8_t *payload, size_t length) {
    ██     ██ ██ ██      ██
    ██  █  ██ ██ █████   ██
    ██ ███ ██ ██ ██      ██
-   ███ ███  ██ ██      ██
+   ███   ███ ██ ██      ██
  */
 /**
  * [setup_wifi description]
@@ -114,14 +200,12 @@ void setup_wifi() {
   Serial.println();
   Serial.print("Connecting to ");
   Serial.println(_ssid);
-
   WiFi.mode(WIFI_STA);
   WiFi.begin(_ssid, _password);
   WiFi.setSleepMode(WIFI_NONE_SLEEP);
   // wifi_set_sleep_type(NONE_SLEEP_T);
-
   while (WiFi.waitForConnectResult() != WL_CONNECTED) {
-    delay(1000);
+    delay(500);
     Serial.print(".");
     // ESP.restart();
     // WiFi.begin(_ssid, _password);
@@ -182,26 +266,27 @@ void setup_MPU_9150() {
  * [setup description]
  */
 void setup() {
-  Serial.begin(115200);
+  Serial.begin(460800);
   // Serial.setDebugOutput(true);
   Serial.println();
   Serial.println();
   Serial.println();
   system_update_cpu_freq(160);
-  // os_timer_setfn(&myTimer, timerCallback, NULL);
+  os_timer_setfn(&myTimer, timerCallback, NULL);
   /*for(uint8_t t = 4; t > 0; t--) {
       Serial.printf("[SETUP] BOOT WAIT %d...\n", t);
       Serial.flush();
       delay(1000);
      }*/
-  setup_BNO055();
-  setup_MPU_9150();
+  if (ABS_IMU_OUT) setup_BNO055();
+  if (IMU_OUT) setup_MPU_9150();
   setup_wifi();
   webSocket.begin(_server, _port);
   // webSocket.setAuthorization("user", "Password"); // HTTP Basic Authorization
   webSocket.onEvent(webSocketEvent);
+  lastMsg = 0;
   delay(500);
-  // os_timer_arm(&myTimer, 10, true);
+  os_timer_arm(&myTimer, 10, true);
 }
 /*
    ██       ██████   ██████  ██████
@@ -214,93 +299,17 @@ void setup() {
  * [loop description]
  */
 void loop() {
-  //  webSocket.loop();
-  // create varibles to strore sensor data
-  now = millis();
-  if (socket_connected){
-
   /*  if ((now - lastmsg) >= refresh_delay) {
-      Serial.println(now - lastmsg);
       lastmsg = now;
-      if (ACC_data) {
-        for (int i = 0; i < 10; i++) {
-          webSocket.sendTXT(data);
-          //sample = 0;
-        }
-      }
-      if (Q_data)
-        webSocket.sendTXT(Quaternion);
-      Q_data = false;
-      ACC_data = false;
-    }*/
-    //Serial.println((lastacc));
-    if(((now - lastacc) >= MPU_9150_delay)) {
-      lastacc = now;
-      ACC_data = true;
-
-      myIMU.readAccelData(myIMU.accelCount); // Read the x/y/z adc values
-      myIMU.getAres();
-      // Now we'll calculate the accleration value into actual g's
-      // This depends on scale being set
-      myIMU.ax = (float)myIMU.accelCount[0] * myIMU.aRes; // - accelBias[0];
-      myIMU.ay = (float)myIMU.accelCount[1] * myIMU.aRes; // - accelBias[1];
-      myIMU.az = (float)myIMU.accelCount[2] * myIMU.aRes; // - accelBias[2];
-      char axbuf[10];
-      char aybuf[10];
-      char azbuf[10];
-      dtostrf(myIMU.ax, 3, 4, axbuf);
-      dtostrf(myIMU.ay, 3, 4, aybuf);
-      dtostrf(myIMU.az, 3, 4, azbuf);
-      sprintf(MPU_ACC, "{\"Accelerometer\":{\"x\":\"%s\",\"y\":\"%s\",\"z\":\"%"
-                       "s\"},\"Time\":\"%i\"}",
-              axbuf, aybuf, azbuf, now);
-      //data = MPU_ACC;
-      webSocket.sendTXT(MPU_ACC);
-      Serial.println(MPU_ACC);
-    }
-
-  /*  if ((now - lastMsg) >= BNO055_SAMPLERATE_DELAY_MS && ABS_IMU_OUT) {
-      lastMsg = now;
-      Q_data = true;
-      sensors_event_t event;
-      bno.getEvent(&event);
-      // imu::Vector<3> lineacc =
-      // bno.getVector(Adafruit_BNO055::VECTOR_LINEARACCEL);
-
-      /* //get euler data
-         float X = event.orientation.x;
-         float Y = event.orientation.y;
-         float Z = event.orientation.z;
-
-         char xbuf[10];
-         char ybuf[10];
-         char zbuf[10];
-         dtostrf( X,3,4,xbuf);
-         dtostrf( Y,3,4,ybuf);
-         dtostrf( Z,3,4,zbuf);
-         sprintf(data, "{\"orientation\":{\"euler\":[%s,%s,%s]}}", xbuf, ybuf,
-         zbuf );
-       */
-
-      // get Quaternion
-    /*  imu::Quaternion quat = bno.getQuat();
-      float qW = quat.w();
-      float qX = quat.x();
-      float qY = quat.y();
-      float qZ = quat.z();
-
-      char wbuf[10];
-      char xbuf[10];
-      char ybuf[10];
-      char zbuf[10];
-      dtostrf(qW, 3, 4, wbuf);
-      dtostrf(qX, 3, 4, xbuf);
-      dtostrf(qY, 3, 4, ybuf);
-      dtostrf(qZ, 3, 4, zbuf);
-      sprintf(Quaternion, "{\"Quaternion\",{\"w\":\"%s\",\"x\":\"%s\",\"y\":\"%"
-                          "s\",\"z\":\"%s\"},\"Time\":\"%i\"}",
-              wbuf, xbuf, ybuf, zbuf, now);
-      // Serial.println(Quaternion);
-    }*/
-  }
+*/
+//while (1);
+/*  if ((((now = millis()) - lastMsg) >= BNO055_SAMPLERATE_DELAY_MS )&& socket_connected) {
+    lastMsg = now;
+    ABS_IMU();
+  }*/
+    /*  if(((now - lastacc) >= MPU_9150_delay)) {
+        lastacc = now;
+        IMU();
+      }*/
+  //yield();
 }
