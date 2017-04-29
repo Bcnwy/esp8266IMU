adbd0da1cc08f36fa772bc38991aed7c348e9403
Bcnwy <benconway@outlook.com>
2017-04-29 17:59:18 +0100

    async websocket dumps every 10 samples BNO055 ONLY

--- src/WebSocketIMU.cpp
+++ src/WebSocketIMU.cpp
@@ -19,15 +19,15 @@ extern "C" {
 #include <Hash.h>
 #include <MPU9250.h>
 #include <WebSocketsClient.h>
-#include <string>
+#include <string.h>
 
 
 #define LED = 2;
-#define BNO055_SAMPLERATE_DELAY_MS (10000)
+#define ABS_IMU_SAMPLERATE_DELAY_MS (100)
 #define MPU_9150_delay (10) // millis between samples 1/freq * 1000
 #define ABS_IMU_OUT true
 #define IMU_OUT false
-#define refresh_delay (10)
+
 
 // Update these with values suitable for your network.
 /*const char *_ssid = "CPT Sensors";
@@ -36,23 +36,33 @@ const char *_server = "192.168.0.102";
 */
 const char *_ssid = "W1F1";
 const char *_password = "B3NR1CHJ0RD4N14N";
-const char *_server = "192.168.0.15";
+const char *_server = "192.168.0.21";
 
 uint16_t _port = 81;
 unsigned  long lastMsg = 0, lastacc = 0, start_loop = 0, end_loop = 0, lastmsg = 0, now = 0;
 bool ACC_data = false, Q_data = false;
 int samples = 0;
 String data, wstr[10], xstr, ystr, zstr;
+char buf[10][4][10];
+long Stime[10];
+
 char Quaternion[50], MPU_ACC[50];
 
 WiFiClient espClient;
 WebSocketsClient webSocket;
 Adafruit_BNO055 bno = Adafruit_BNO055(); // Init Sensor
-MPU9250 myIMU;
+//MPU9250 myIMU;
 
-os_timer_t myTimer;
-bool socket_connected = false;
+os_timer_t ABS_IMU_timer;
+volatile bool socket_connected = false;
 void ABS_IMU();
+
+typedef enum {
+        wAxis = 0,
+        xAxis = 1,
+        yAxis = 2,
+        zAxis = 3,
+} Axis;
 /*
 ██ ███    ██ ████████ ███████ ██████  ██████  ██    ██ ██████  ████████
 ██ ████   ██    ██    ██      ██   ██ ██   ██ ██    ██ ██   ██    ██
@@ -64,7 +74,7 @@ void ABS_IMU();
  * [timerCallback description]
  * @param pArg [description]
  */
-void timerCallback(void *pArg) {
+void ABS_IMU_timerCB(void *pArg) {
     ABS_IMU();
 }
 /*
@@ -79,38 +89,20 @@ void timerCallback(void *pArg) {
  */
 void ABS_IMU(void){
   Q_data = true;
-  //sensors_event_t event;
-  //bno.getEvent(&event);
-  // imu::Vector<3> lineacc =
-  // bno.getVector(Adafruit_BNO055::VECTOR_LINEARACCEL);
-  /* //get euler data
-     float X = event.orientation.x;
-     float Y = event.orientation.y;
-     float Z = event.orientation.z;
-
-     char xbuf[10];
-     char ybuf[10];
-     char zbuf[10];
-     dtostrf( X,3,4,xbuf);
-     dtostrf( Y,3,4,ybuf);
-     dtostrf( Z,3,4,zbuf);
-     sprintf(data, "{\"orientation\":{\"euler\":[%s,%s,%s]}}", xbuf, ybuf,
-     zbuf );
-   */
   // get Quaternion
   imu::Quaternion quat = bno.getQuat();
-  //sTime = millis();
+  Stime[samples] = millis();
   float qW = quat.w();
   float qX = quat.x();
   float qY = quat.y();
   float qZ = quat.z();
-  char wbuf[10], xbuf[10], ybuf[10], zbuf[10];
+  //char wbuf[10], xbuf[10], ybuf[10], zbuf[10];
 
-  dtostrf(qW, 6, 2, wbuf);
-  dtostrf(qX, 6, 2, xbuf);
-  dtostrf(qY, 6, 2, ybuf);
-  dtostrf(qZ, 6, 2, zbuf);
-  wstr[samples]=  (String)wbuf + ',';
+  dtostrf(qW, 5, 2, buf[samples][wAxis]);
+  dtostrf(qX, 5, 2, buf[samples][xAxis]);
+  dtostrf(qY, 6, 2, buf[samples][yAxis]);
+  dtostrf(qZ, 6, 2, buf[samples][zAxis]);
+  samples++;
   //xstr +=  (String)xbuf + ',';
   //ystr +=  (String)ybuf + ',';
   //zstr +=  (String)zbuf + ',';
@@ -130,7 +122,8 @@ void ABS_IMU(void){
 /**
  * [IMU description]
  */
-void IMU(){
+/*
+void IMU(){}
   ACC_data = true;
   myIMU.readAccelData(myIMU.accelCount); // Read the x/y/z adc values
   myIMU.getAres();
@@ -152,6 +145,7 @@ void IMU(){
   //webSocket.sendTXT(MPU_ACC);
   //Serial.println(MPU_ACC);
 }
+*/
 /*
 ██     ██ ███████ ██████  ███████  ██████   ██████ ██   ██ ███████ ████████
 ██     ██ ██      ██   ██ ██      ██    ██ ██      ██  ██  ██         ██
@@ -259,6 +253,7 @@ void setup_BNO055() {
 /**
  * [setup_MPU_9150 description]
  */
+/*
 void setup_MPU_9150() {
   myIMU.initMPU9250();
   // Initialize device for active mode read of acclerometer, gyroscope, and
@@ -267,6 +262,7 @@ void setup_MPU_9150() {
   myIMU.calibrateMPU9250(myIMU.gyroBias, myIMU.accelBias);
   myIMU.initAK8963(myIMU.magCalibration);
 }
+*/
 /*
 ███████ ███████ ████████ ██    ██ ██████
 ██      ██         ██    ██    ██ ██   ██
@@ -285,22 +281,21 @@ void setup() {
   Serial.println();
   system_update_cpu_freq(80);
   //os_update_cpu_frequency(160);
-  os_timer_setfn(&myTimer, timerCallback, NULL);
+  os_timer_setfn(&ABS_IMU_timer, ABS_IMU_timerCB, NULL);
   /*for(uint8_t t = 4; t > 0; t--) {
       Serial.printf("[SETUP] BOOT WAIT %d...\n", t);
       Serial.flush();
       delay(1000);
      }*/
   if (ABS_IMU_OUT) setup_BNO055();
-  if (IMU_OUT) setup_MPU_9150();
+  //if (IMU_OUT) setup_MPU_9150();
   setup_wifi();
   webSocket.begin(_server, _port);
-
   // webSocket.setAuthorization("user", "Password"); // HTTP Basic Authorization
   webSocket.onEvent(webSocketEvent);
   //lastMsg = 0;
   delay(500);
-  os_timer_arm(&myTimer, 100, true);
+  os_timer_arm(&ABS_IMU_timer, ABS_IMU_SAMPLERATE_DELAY_MS, true);
   Serial.println("Boot Done...");
 }
 /*
@@ -314,33 +309,32 @@ void setup() {
  * [loop description]
  */
 void loop() {
-  if (socket_connected){
-    if ((++samples)>=10){
-      samples = 0;
-      for(int i=0;i<10;i++)
+  //if (socket_connected){
+    if (samples>=10){
+      for(int i=0;i<samples;i++)
       {
         data = "{\"Quaternion\":{";
         data += "\"w\":[";
-        data += wstr[i];
+        data += buf[i][wAxis];
         data += "],\"x\":[";
+        data += buf[i][xAxis];
       //  data += xstr[i];
         data += "],\"y\":[";
+        data += buf[i][yAxis];
         //data += ystr[i];
         data += "],\"z\":[";
+        data += buf[i][zAxis];
         //data += zstr[i];
-        data += "]},\"Time\":[]}";
-        //webSocket.sendTXT(data);
-        Serial.println(data);
+        data += "]},\"Time\":[";
+        data += Stime[i];
+        data += "]}";
+        webSocket.sendTXT(data);
+      Serial.println("sent..");
       }
-      /*wstr = "";
-      xstr = "";
-      ystr = "";
-      zstr = "";*/
-      //data += sTime;
-
-      //Serial.println(data);
-    }
+      samples = 0;
+    //}
   }
+  //yield();
 //if (!socket_connected)webSocket.begin(_server, _port);
   /*  if ((now - lastmsg) >= refresh_delay) {
       lastmsg = now;
@@ -356,5 +350,5 @@ void loop() {
       }*/
   //delay(500);
   //Serial.print('.');
-  yield();
+
 }
